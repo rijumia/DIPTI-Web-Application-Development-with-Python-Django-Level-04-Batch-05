22f457b766eade37aa806a1c14b6768c04049e2b
rijumia <riju.mia2612@gmail.com>
2025-07-11 17:30:01 +0600

    Add initial files and project documentation for Institute Management System

--- Django_Practice/IMS/Institute_Management_System/UserAuthApp/views.py
+++ Django_Practice/IMS/Institute_Management_System/UserAuthApp/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from UserAuthApp.models import *
+from django.contrib.auth import authenticate, login, logout
