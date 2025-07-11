533423a07d7f9b782121a2f9b1cebc3f3756a503
rijumia <riju.mia2612@gmail.com>
2025-07-11 12:12:32 +0600

    New File Add

---
+++ Django_Practice/SMS/student_management_system/studentManagementSystemApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudentmanagementsystemappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'studentManagementSystemApp'
