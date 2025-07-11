57a9ed5f773f2113ac0605202d656af8ab07959a
rijumia <riju.mia2612@gmail.com>
2025-07-11 18:09:19 +0600

    Implement user registration and login functionality with templates and views

--- Django_Practice/IMS/Institute_Management_System/UserAuthApp/urls.py
+++ Django_Practice/IMS/Institute_Management_System/UserAuthApp/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from UserAuthApp.views import *
+
+urlpatterns = [
+    path('',registerPage, name='registerPage'),
+    path('loginPage/',loginPage, name='loginPage'),
+]
