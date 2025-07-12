957702b5e7b2087a76f4f45be37299979e0ca8d9
rijumia <riju.mia2612@gmail.com>
2025-07-12 19:37:35 +0600

    Apply .gitignore rules

--- Django_Practice/IMS/Institute_Management_System/CourseApp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from UserAuthApp.models import TeacherModel, StudentModel
-
-
-class CourseModel(models.Model):
-    course_title = models.CharField(max_length=100,null=True)
