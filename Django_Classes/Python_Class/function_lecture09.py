ef3a6413bcdb9a6a171ab9e629225c2b1ca45fb1
Riju Mia <riju.mia2612@gmail.com>
2025-07-13 12:39:34 +0600

    Create a function with three parameter and print four time first empty, second print two number sum three number sum, fourth sum four number

--- Django_Classes/Python_Class/function_lecture09.py
+++ Django_Classes/Python_Class/function_lecture09.py
@@ -6,7 +6,7 @@ greet('Mr Admin')
 greet()
 
 
-
+#Create a function with three parameter and print four time first empty, second print two number sum three number sum, fourth sum four number
 def Num(num1=0, num2=0, num3=0):
     print('Sum Num: ',num1+num2+num3)
     
