6df6ea03d97b113811a5d10992ef6b3774a54d48
Riju Mia <riju.mia2612@gmail.com>
2025-07-14 11:15:29 +0600

    Adding a placeholder to a Django form field allows you to display a hint or example text within the input field that disappears when the user starts typing. This enhances user experience by providing guidance on the expected input.

--- Django_Classes/Django_Class/DjangoForm/DjangoForm/FormApp/forms.py
+++ Django_Classes/Django_Class/DjangoForm/DjangoForm/FormApp/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
 class UserInfoForm(forms.Form):
-    FullName = forms.CharField(max_length=50)
+    FullName = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your full name'
+        })
+    )
     email = forms.EmailField()
     phone = forms.IntegerField()
     addrees = forms.CharField(max_length=50)
