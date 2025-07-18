d4f1f65ad051f6c72417faa8ed3afedca728fdbf
rijumia <riju.mia2612@gmail.com>
2025-07-18 17:02:12 +0600

    model modify

--- Django_Classes/Django_Class/DjangoForm/DjangoForm/FormApp/models.py
+++ Django_Classes/Django_Class/DjangoForm/DjangoForm/FormApp/models.py
@@ -9,7 +9,7 @@ class UserInfoModels(models.Model):
     image = models.ImageField(null=True)
     
     def __str__(self):
-        return self.FullName
+        return self.email
     
 class UserInfoModelsTwo(models.Model):
     FullName = models.CharField(max_length=50,null=True)
