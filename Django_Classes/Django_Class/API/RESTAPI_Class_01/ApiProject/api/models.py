a99c2bf98b87adb803c1876c70efd32eb083df2e
Riju Mia <riju.mia2612@gmail.com>
2025-07-29 12:42:04 +0600

    'error'

--- Django_Classes/Django_Class/API/RESTAPI_Class_01/ApiProject/api/models.py
+++ Django_Classes/Django_Class/API/RESTAPI_Class_01/ApiProject/api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class StudentModel(models.Model):
+    studentRoll = models.PositiveIntegerField(null=True)
+    studentUsername = models.CharField(max_length=50, null=True)
     studentName = models.CharField(max_length=100, null=True)
     studentAddress = models.TextField(null=True)
     studentAge = models.IntegerField(null=True)
--- Django_Classes/Django_Class/API/RESTAPI_Class_01/ApiProject/api/serializers.py
+++ Django_Classes/Django_Class/API/RESTAPI_Class_01/ApiProject/api/serializers.py
@@ -6,3 +6,17 @@ class StudentSerializer(serializers.ModelSerializer):
         model = StudentModel
         
         fields = '__all__'
+    
+    def create(self, validated_data):
+        student_name = validated_data.get('studentName')
+        student = StudentModel.objects.order_by('-studentRoll').first()
+    
+        if student:
+            roll = student.studentRoll + 1
+        else:
+            roll = 1
+        instance_data = StudentModel.objects.create(**validated_data)
+        instance_data.studentRoll = roll
+        instance_data.studentUsername = student_name + str(roll)
+        instance_data.save()
+        return instance_data
