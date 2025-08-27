06f560d5c3aa7f80079b8bc5a8897418ed23e316
rijumia <riju.mia2612@gmail.com>
2025-08-27 19:17:21 +0600

    create UserProfileModel

--- Django_Practice/Calorie_Counter/Riju_17_CalorieCounter/CalorieCounterApp/models.py
+++ Django_Practice/Calorie_Counter/Riju_17_CalorieCounter/CalorieCounterApp/models.py
@@ -7,3 +7,17 @@ class CustomUserAuthModel(AbstractUser):
     def __str__(self):
         return self.username
     
+class UserProfileModel(models.Model):
+    user = models.OneToOneField(CustomUserAuthModel, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200, null=True)
+    age = models.PositiveIntegerField(null=True)
+    gender = models.CharField(choices=[
+        ('Male', "Male"),
+        ('Female', 'Female'),
+    ], max_length=10, null=True)
+    height = models.FloatField(null=True)
+    weight = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+    
